633ed9d5df925bd4781514f7c5b658d5e6f1de18
kardasjan <jan.kardas@seznam.cz>
2017-03-02 08:40:33 +0100

    Add Paradox support

---
+++ format_cs_cz_paradox.go
@@ -0,0 +1,55 @@
+package monday
+
+// ============================================================
+// Format rules for "cs_CZ_Paradoxcritic" locale: Czech (Czech Republic)
+// ============================================================
+
+var longDayNamesCsCZParadox = map[string]string{
+	"Sunday":    "nedele",
+	"Monday":    "pondeli",
+	"Tuesday":   "utery",
+	"Wednesday": "streda",
+	"Thursday":  "ctvrtek",
+	"Friday":    "patek",
+	"Saturday":  "sobota",
+}
+
+var shortDayNamesCsCZParadox = map[string]string{
+	"Sun": "ne",
+	"Mon": "po",
+	"Tue": "ut",
+	"Wed": "st",
+	"Thu": "ct",
+	"Fri": "pa",
+	"Sat": "so",
+}
+
+var longMonthNamesCsCZParadox = map[string]string{
+	"January":   "led",
+	"February":  "uno",
+	"March":     "breze",
+	"April":     "duben",
+	"May":       "kveten",
+	"June":      "cerven",
+	"July":      "cervenec",
+	"August":    "srpen",
+	"September": "zari",
+	"October":   "rijen",
+	"November":  "listopad",
+	"December":  "prosinec",
+}
+
+var shortMonthNamesCsCZParadox = map[string]string{
+	"Jan": "led",
+	"Feb": "uno",
+	"Mar": "bre",
+	"Apr": "dub",
+	"May": "kve",
+	"Jun": "cvn",
+	"Jul": "cvc",
+	"Aug": "srp",
+	"Sep": "zar",
+	"Oct": "rij",
+	"Nov": "lis",
+	"Dec": "pro",
+}
